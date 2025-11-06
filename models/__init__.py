82c518a59c06d623cbd636474796d96089267314
nihaanth-vulupala <nvulupala2024@fau.edu>
2025-11-05 19:05:18 -0500

    Merge remote changes

---
+++ models/__init__.py
@@ -0,0 +1,13 @@
+"""
+Model loading and initialization for SOTA Small Networks Comparison
+
+This module provides unified interface for loading:
+- MobileNetV3-Small
+- EfficientNet-B0
+- ShuffleNetV2
+- SqueezeNet
+
+To be implemented in Week 2.
+"""
+
+# To be implemented
